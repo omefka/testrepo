f677b48dace75619b6843de595132223c239d07c
Omer Faruk KULU <76488427+omefka@users.noreply.github.com>
2023-04-13 18:56:21 +0300

    Add files via upload

---
+++ frenchchef.py
@@ -0,0 +1,11 @@
+from chef import Chef
+
+class FrenchChef(Chef):
+    #def make_chicken(self):
+     #   print("This Chef makes chicken")
+   # def make_salad(self):
+   #     print("this Chef makes salad")
+   def make_special_dish(self):
+       print("This Chef makes special beef rosto ")
+   def make_fried_rice(self):
+        print("The chef makes fried rice")
